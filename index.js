bced591f0afa5d8884cf89b763ae60f404e5c5f9
pranav Malwad <malwadpranav@gmail.com>
2024-05-19 15:46:24 +0530

    changes in the backend

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ app.use(
   })
 );
 
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://learnifycom.netlify.app"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader(
+//     "Access-Control-Allow-Origin",
+//     "https://learnifycom.netlify.app"
+//   );
+//   res.setHeader("Access-Control-Allow-Credentials", "true");
+//   next();
+// });
 
 const secretKey = process.env.SECRETKEY;
 
